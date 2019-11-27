0ab392b4911738a73117c093e18f906c5ddd2e56
assemtolen <43029664+assemtolen@users.noreply.github.com>
2019-11-28 00:38:54 +0600

    Update end.php

--- end.php
+++ end.php
@@ -9,7 +9,7 @@
 	$query_result = mysqli_query($conn, $query);
 	$all = mysqli_fetch_assoc($query_result);
 	$q = (isset($_POST["specialty"]) ? $_POST["specialty"] : '');
-	$q = "INSERT INTO survey1 (`university`, `specialty`, `social_life`, `study_load`, `exchange`, `equipped_laboratories`, `education_lang`, `work_by_profession`) VALUES ('".$all['university']."', '".$all['specialty']."',".$_POST['social_life'].",".$_POST['study_load'].",".$_POST['exchange'].",".$_POST['equipped_laboratories'].",".$_POST['work_by_profession'].",".$_POST['work_by_profession'].")";
+	$q = "INSERT INTO survey1 (`university`, `specialty`, `social_life`, `study_load`, `exchange`, `equipped_laboratories`, `education_lang`, `work_by_profession`) VALUES ('".$all['university']."', '".$all['speciality_name']."',".$_POST['social_life'].",".$_POST['study_load'].",".$_POST['exchange'].",".$_POST['equipped_laboratories'].",".$_POST['work_by_profession'].",".$_POST['work_by_profession'].")";
 	$result = mysqli_query($conn,$q) or die (mysqli_error ($conn));
 
 ?>
