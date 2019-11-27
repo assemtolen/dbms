4238b8059d6b0f3a72243d5eed83913eeec9bd48
assemtolen <43029664+assemtolen@users.noreply.github.com>
2019-11-27 23:59:43 +0600

    Update input.php

--- input.php
+++ input.php
@@ -9,7 +9,7 @@ if(!mysqli_set_charset($conn, $charset)){
 #$result1 = mysqli_query($conn, $query);
 $options = "";
 
-$row_query = 'SELECT DISTINCT university, uni_index FROM info1';
+$row_query = 'SELECT DISTINCT university, uni_index FROM info2';
 $row_result = mysqli_query($conn,$row_query);
 while ($row = mysqli_fetch_array($row_result)) {
 	$options = $options.'<option value = '.$row[1].' style ="width:80%;">'.$row[0].'</option>';
@@ -23,23 +23,30 @@ while ($row = mysqli_fetch_array($row_result)) {
 	<meta charset="utf-8">
 	<title>survey</title>
 	
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<script>
-		
-	</script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+ <link rel="stylesheet" href="./survey_files/style.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.23.0/slimselect.min.css">
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.23.0/slimselect.min.js"></script>
+ <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
 <body>
 	<div class="select_cont" id='select_cont'>
+	<p style="color: white; font-size: 24px; font-style: normal;text-align: center;">В каком вузе учились/учитесь?</p>
+	<br>
 	<form action="survey.php" method="GET">
-		<select class="custom-select d-block w-100" required="" name='code' class="chosen">
+		<select class="custom-select" required="" name='code' required>
         	<div class="div_option"><?php echo $options; ?></div>
     	</select>
-    	<div class="div_sub"><input  type="submit" name="submit" class="submit"></input></div>
+    	<div class="div_sub"><button  type="submit" name="submit" class="submit">отправить</button></div>
     </form>
     </div>
 
 </body>
-
+<script type="text/javascript">
+new SlimSelect({
+  select: '.custom-select'
+}) 
+</script>
 </html>
